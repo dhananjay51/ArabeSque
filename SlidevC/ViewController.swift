0ff2a69db72722f8ea33a099cf6d00110cf31f7f
dhananjay <dhanajaysingh53@gmail.com>
2018-06-20 00:47:32 +0530

    Initial Commit

---
+++ SlidevC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlidevC
+//
+//  Created by Macbook Pro on 30/03/1940 Saka.
+//  Copyright © 1940 Saka Macbook Pro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
